178ed23cc3ce88ba5a2f4cb608c812802f38f8f6
Sunghwan Kim <sunghwan@scatterlab.co.kr>
2021-09-22 19:21:40 +0900

    refactor: merge into updatefilters

--- SimpleEQ/Source/PluginProcessor.h
+++ SimpleEQ/Source/PluginProcessor.h
@@ -120,6 +120,11 @@ private:
                 updateCutFilterElement<Slope_12>(lowCut, cutCoefficients);
         }
     }
+    
+    void updateLowCutFilters(const ChainSettings& chainSettings);
+    void updateHighCutFilters(const ChainSettings& chainSettings);
+    
+    void updateFilters();
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
